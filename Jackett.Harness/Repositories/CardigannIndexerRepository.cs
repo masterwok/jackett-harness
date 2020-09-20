db2057476394d0cc1702643bbab00d08448e9058
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2020-09-20 17:42:39 -0400

    Updated Jackett

--- Jackett.Harness/Repositories/CardigannIndexerRepository.cs
+++ Jackett.Harness/Repositories/CardigannIndexerRepository.cs
@@ -65,25 +65,6 @@ namespace Jackett.Harness.Repositories
         {
             Mapper.Initialize(cfg =>
             {
-                cfg.CreateMap<WebClientByteResult, WebClientStringResult>()
-                    .ForMember(x => x.Content, opt => opt.Ignore()).AfterMap((be, str) =>
-                    {
-                        var encoding = be.Request.Encoding ?? Encoding.UTF8;
-                        str.Content = encoding.GetString(be.Content);
-                    });
-
-                cfg.CreateMap<WebClientStringResult, WebClientByteResult>()
-                    .ForMember(x => x.Content, opt => opt.Ignore()).AfterMap((str, be) =>
-                    {
-                        if (!string.IsNullOrEmpty(str.Content))
-                        {
-                            var encoding = str.Request.Encoding ?? Encoding.UTF8;
-                            be.Content = encoding.GetBytes(str.Content);
-                        }
-                    });
-
-                cfg.CreateMap<WebClientStringResult, WebClientStringResult>();
-                cfg.CreateMap<WebClientByteResult, WebClientByteResult>();
                 cfg.CreateMap<ReleaseInfo, ReleaseInfo>();
 
                 cfg.CreateMap<ReleaseInfo, TrackerCacheResult>().AfterMap((r, t) =>
